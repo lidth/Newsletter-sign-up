6d38249a914de6dc642cfb94194eeb400560633f
Random <155200367+lidth@users.noreply.github.com>
2024-01-21 18:28:16 -0300

    Initial commit

---
+++ assets/jais.js
@@ -0,0 +1,9 @@
+function subscribe() {
+    let email = document.querySelector('input#imail').value
+}
+
+function sucessp() {
+    subscribe()
+    let stexto = document.querySelector('div#sp')
+    stexto.innerHTML = email
+}
